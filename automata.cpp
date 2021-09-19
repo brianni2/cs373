be51d981400918234ffb8274bf31043aa18cdf2d
brianni2 <54452625+brianni2@users.noreply.github.com>
2021-09-18 20:26:20 -0400

    Update automata.cpp

--- automata.cpp
+++ automata.cpp
@@ -29,45 +29,70 @@ int main(int argc, char* argv[]) {
     vector<int> acceptState;
     
     ifstream inFS;
-
-    while(inFS.is_open()) {
-        cin >> readState;
-        cin >> numState;
-        if(readState.find("state") == true) {
-            getline(cin, specState);
-            if(specState.find("start") == true) {
+    inFS.open(fileName);
+    
+    inFS >> readState;
+    inFS >> numState;
+    while(!inFS.fail()) {
+        if(readState.find("state") != string::npos) {
+            getline(inFS, specState);
+            if(specState.find("start") != string::npos) {
                 startState = numState;
             }
-            if(specState.find("accept") == true) {
+            if(specState.find("accept") != string::npos) {
                 acceptState.push_back(numState);
+                //cout << acceptState.at(acceptState.size() - 1);
             }
         }
-        
-        if(readState.find("transition") == true) {
-            cin >> readInput;
-            cin >> readMoveState;
+        if(readState.find("transition") != string::npos) {
+            inFS >> readInput;
+            inFS >> readMoveState;
             automata[numState].input.push_back(readInput);
             automata[numState].moveState.push_back(readMoveState);
         }
+        inFS >> readState;
+        inFS >> numState;
     }
+    inFS.close();
     
     int currState = startState;
+    vector<int> acceptInput;
+    vector<int> rejectInput;
     
     for(int z = 0; z < strInput.length(); z++) {
         for(int i = 0; i < automata[currState].input.size(); i++) {
             if(automata[currState].input.at(i) == strInput[z]) {
-                for(int j = 0; j < acceptState.size(); j++) {
-                    if(currState == acceptState.at(j)) {
-                        if(automata[currState].moveState.at(i) == currState) {
+                if(automata[currState].moveState.at(i) == currState) {
+                    int vecSize = acceptState.size();
+                    for(int j = 0; j < acceptState.size(); j++) {
+                        if(currState == acceptState.at(j)) {
+                            acceptInput.push_back(currState);
                             acceptState.erase(std::remove(acceptState.begin(), acceptState.end(), currState), acceptState.end());
+                            break;
                         }
                     }
+                    if(acceptState.size() == vecSize) {
+                        rejectInput.push_back(currState);
+                    }
                 }
+                currState = automata[currState].moveState.at(i);
             }
         }
     }
-    
-    cout << "reject " << acceptState << endl;
+
+    if(acceptInput.empty()) {
+        cout << "reject ";
+        for(int b = 0; b < rejectInput.size(); b++) {
+            cout << rejectInput.at(b) << " ";
+        }
+    }
+    if(!acceptInput.empty()) {
+        cout << "accept ";
+        for(int b = 0; b < acceptInput.size(); b++) {
+            cout << acceptInput.at(b) << " ";
+        }
+    }
+    cout << endl;
     
     return 0;
 }
